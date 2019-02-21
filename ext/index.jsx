81a6f020e7acc44cdc1a75c60d4ea2fb823e6d6b
Abel Lopes <abellopes@gmail.com>
2019-02-20 22:40:59 -0300

    [WEBPACK] corrigido bug

--- ext/index.jsx
+++ ext/index.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom'
 
 
 ReactDOM.render(
-
-        <h1>TESTE</h1>
-    
+        <h1>TESTE</h1>    
     , document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                pugins: ['transform-object-rest-spread']
+                plugins: ['transform-object-rest-spread']
             }
         }]
     }
